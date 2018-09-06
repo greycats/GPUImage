0759a597f5cc1822a4fe5c5ede8effb464c5b60c
Travel.Chu <chuchuanming@gmail.com>
2018-09-06 15:22:49 +0800

    setting copyNextSampleBuffer audio format to PCM and add play mvoie audio
    https://github.com/BradLarson/GPUImage/pull/1689

---
+++ framework/Source/GPUAudioPlayer.h
@@ -0,0 +1,17 @@
+//
+//  GPUAudioPlayer.h
+//  GPUImage
+//
+//  Created by Travel Chu on 9/6/18.
+//  Copyright © 2018 Brad Larson. All rights reserved.
+//
+
+#import <GPUImage/GPUImage.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GPUAudioPlayer : GPUImageMovieWriter
+- (void)stopPlay;
+@end
+
+NS_ASSUME_NONNULL_END
